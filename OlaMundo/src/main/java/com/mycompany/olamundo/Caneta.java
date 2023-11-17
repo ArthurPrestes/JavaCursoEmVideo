1422c6dae96b25bbf46de655a661607f282b51c0
Arthur Prestes <arthur.prestes18@gmail.com>
2023-11-17 16:00:31 -0300

    Primeiro commit

---
+++ OlaMundo/src/main/java/com/mycompany/olamundo/Caneta.java
@@ -0,0 +1,20 @@
+package com.mycompany.olamundo;
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    
+    void rabiscar(){
+        
+    }
+    
+    void tampar(){
+        
+    }
+    
+    void destampar(){
+        
+    }
+}
